4d48218a08f357332f4caaa9b7cae196aa5ac7e9
Wasif Khan <waasif.khaan@gmail.com>
2018-05-07 19:07:46 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,9 @@
 def reverse_each_word(sentence)
+  result = []
   array = sentence.split
-  array.each do |element|
-    element.reverse 
+    array.each do |element|
+      result << element.reverse 
+    end
+    result
+  end
   
